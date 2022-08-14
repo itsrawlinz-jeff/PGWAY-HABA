40325d727a23d5b4186c6c8c0fb937d215246a18
Jeffrey Rowlings <71753362+itsrawlinz-jeff@users.noreply.github.com>
2022-08-14 08:38:15 +0300

    Update dbconnection.php

--- LOGIN/loginsystem/admin/dbconnection.php
+++ LOGIN/loginsystem/admin/dbconnection.php
@@ -1,14 +1,14 @@
 <?php
 define('DB_SERVER','localhost');
-define('DB_USER','root');
-define('DB_PASS' ,'');
-define('DB_NAME', 'loginsystem');
+define('DB_USER','habadati_login');
+define('DB_PASS' ,'--%FT1RR7cm{');
+define('DB_NAME', 'habadati_login');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 
 // Check connection
 if (mysqli_connect_errno())
 {
-echo "Failed to connect to MySQL: " . mysqli_connect_error();
+echo "Failed to connect to Your MySQL: " . mysqli_connect_error();
  }
 
 ?>
